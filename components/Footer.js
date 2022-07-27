93d7bd72feca86987b1a96d4738c347615b3bf40
Oliver Armstrong <osarmstrong@gmail.com>
2022-07-27 12:56:15 +1200

    Content

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,10 @@
 
 export const Footer = () => {
   return (
-    <div style={{height: "3em", zIndex:"9", boxShadow: "3px 0px 5px #00000054"}} className="d-flex flex-row justify-content-center align-items-center">
-        Copyright © Oliver Armstrong {new Date().getFullYear()}
-    </div>
+      <footer style={{height: "3em", zIndex:"9", boxShadow: "3px 0px 5px #00000054"}} className="d-flex flex-row justify-content-center align-items-center">
+          Copyright © Oliver Armstrong {new Date().getFullYear()}
+      </footer>
+    
   )
 }
 
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -3,13 +3,13 @@
 export const ProjectCard = (props) => {
     const {project, showDetails} = props;
     return ( 
-        <div className="showcase-container mt-5 d-flex w-100">
+        <div className="showcase-container mt-5 d-flex flex-column justify-content-start align-items-center w-100">
          <div className="content px-4 d-flex flex-column justify-content-start align-items-center ">
          <h2 className="mb-3">{project.title}</h2>
-               { !showDetails ? <p className="w-50 text-center">{project.blurb}</p> :<p className="w-75 text-left">{project.details}</p>}
+               { !showDetails ? <p className="w-50 text-center">{project.blurb}</p> :<p className="w-50 px-4 text-left">{project.details}</p>}
                
-               <div className="image-container">
-                  <img  style={{marginBottom: "-1em"}} className="w-100" src={project.image} alt={project.title} />
+               <div className="image-container w-75">
+                  <img  style={{marginTop: "-9%"}} className="w-100" src={project.image} alt={project.title} />
                </div>
            <div className="buttons mb-5 text-uppercase ">
            {showDetails ? <a href={`/#projects`} className="btn btn-primary mx-3" role={"button"} rel="noreferrer">Go Back</a> 
